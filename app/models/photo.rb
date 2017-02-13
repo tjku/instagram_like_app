3e7cb9e2a5ec00f614ed8c611be484b0516cd72d
tjku <tomaszjankuc@gmail.com>
2017-02-13 23:56:39 +0100

    Update of photos' layout on front page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '200x200#' }
+  has_attached_file :image, styles: { medium: '1000>', thumb: '300>' }
 
   validates_attachment :image, presence: true,
                                content_type: { content_type: %r{\Aimage\/.*\z} }
